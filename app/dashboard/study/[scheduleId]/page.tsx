a33fcd7aaf3ab099f79f323645a1c31ff6fe5720
vans-wq <van.s@minebea.co.th>
2026-03-08 21:42:21 +0700

    Update page.tsx

--- app/dashboard/study/[scheduleId]/page.tsx
+++ app/dashboard/study/[scheduleId]/page.tsx
@@ -55,7 +55,7 @@ export default function StudyPage() {
 
       <button
         onClick={() => setShowForm(true)}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+        className="bg-blue-500 text-white px-4 py-2 rounded "
       >
         Add Study Session
       </button>
@@ -63,7 +63,7 @@ export default function StudyPage() {
       {showForm && (
         <div className="mt-6 border p-4 rounded">
 
-          <h2 className="font-semibold mb-3">
+          <h2 className="font-semibold mb-3 text-black">
             New Study Session
           </h2>
 
@@ -71,7 +71,7 @@ export default function StudyPage() {
             type="date"
             value={studyDate}
             onChange={(e) => setStudyDate(e.target.value)}
-            className="border p-2 w-full mb-3"
+             className="border-3 border-blue-500 rounded p-2 w-full mb-1 text-blue-700 placeholder-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300"
           />
 
           <input
@@ -79,19 +79,19 @@ export default function StudyPage() {
             placeholder="Minutes"
             value={duration}
             onChange={(e) => setDuration(e.target.value)}
-            className="border p-2 w-full mb-3"
+            className="border-3 border-blue-500 rounded p-2 w-full mb-1 text-blue-700 placeholder-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300"
           />
 
           <textarea
             placeholder="What did you study?"
             value={note}
             onChange={(e) => setNote(e.target.value)}
-            className="border p-2 w-full mb-3"
+             className="border-3 border-blue-500 rounded p-2 w-full mb-1 text-blue-700 placeholder-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300"
           />
 
           <button
             onClick={handleSave}
-            className="bg-green-500 text-white px-3 py-1 rounded"
+            className="bg-green-500 text-black px-3 py-1 rounded"
           >
             Save
           </button>
@@ -101,5 +101,4 @@ export default function StudyPage() {
 
     </div>
   );
-
 }
